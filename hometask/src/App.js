da343e3ca030131feb8808759ac8b3db849a475f
Akshay Goyal <akkigoyal07@gmail.com>
2020-07-25 14:22:25 +0200

    Home Task 1.

--- hometask/src/App.js
+++ hometask/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HelloComponent from './HelloComponent';
+import HelloPureComponent from './HelloPureComponent';
+
+const welcomeMessage = React.createElement(
+	'h1', 
+	{
+		className: 'welcome',
+		align: 'center',
+		style: 
+		{
+			color:'blue'
+		}
+	}, 
+	'Hello World!'
+);
+const gitHubLink = React.createElement(
+	'a', 
+	{ href: "https://github.com/Akshay-Goyal-17/ReactJS-Mentoring.git" }, 
+	'Akshay\'s GitHub'
+);
+
+function ShowGitHubLink(props) {
+  return (
+	<p style={{marginLeft:'20px'}}>
+		Please find my work here: {props.link}
+	</p>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	[welcomeMessage,
+	<table className="table" style={{marginLeft:'20px'}}>
+	<tbody>
+		<HelloComponent/>
+		<HelloPureComponent/>
+	</tbody>
+	</table>,
+	<ShowGitHubLink link={gitHubLink}/>]
+);
 }
 
 export default App;
